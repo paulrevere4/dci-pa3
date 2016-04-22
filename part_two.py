0303a512decf4c62ee6526c794f11ae5bc304797
William Rory Kronmiller <wrk961@gmail.com>
2016-04-21 23:49:24 -0400

    Tuples instead of separate coordinates

--- part_two.py
+++ part_two.py
@@ -27,8 +27,7 @@ def initialize(channel, grid):
 #@puresignal
 def slave_process(channel, old_grid):
   """Main worker process"""
-  x = channel.receive()
-  y = channel.receive()
+  (x, y) = channel.receive()
   if x == 0 or y == 0 or x == len(old_grid[0]) - 1 or y == len(old_grid) - 1:
     # Edge squares remain in steady state
     channel.send(old_grid[y][x])
@@ -67,37 +66,36 @@ def result_printer(grid):
       f.write("%d\n" % (x))
 
 def iterate(gw, iteration, max_iterations, grid, max_y_arg, max_x_arg):
-  print("Started iteration", iteration)
-  # Check if finished
-  if iteration == max_iterations:
-    print("DONE")
-    result_printer(grid)
-    # gw.remote_exec(result_printer, grid=HEAT_GRID) #TODO: change to remote_exec
-    return grid
-  print ("IAMHERE")
-  slaves = []
-  slaves_cords = []
-  #Spawn slave processes with old grid state
-  for y in range(max_y_arg):
-    for x in range(max_x_arg):
-      print ("IAMHERE-looping")
-      # slave = puresignal(slave_process)(grid, x, y) #TODO: Change this to remote_exec
-      slave = gw.remote_exec(slave_process, old_grid=grid)
-      # print ("IAMHERE-looping2")
-      slave.send(x)
-      slave.send(y)
-      slaves.append(slave)
-      slaves_cords.append((x, y))
-  print("Launched workers. Waiting on results")
-  # Update state array
-  for i in range(len(slaves)):
-    slave = slaves[i]
-    x, y = slaves_cords[i]
-    val = slave.receive() #TODO: CHange this to receive
-    grid[y][x] = val
+  while iteration < max_iterations:
+    print("Started iteration", iteration)
+    # Check if finished
+    if iteration == max_iterations:
+      print("DONE")
+      result_printer(grid)
+      # gw.remote_exec(result_printer, grid=HEAT_GRID) #TODO: change to remote_exec
+      return grid
+    print ("IAMHERE")
+    slaves = []
+    slaves_cords = []
+    #Spawn slave processes with old grid state
+    for y in range(max_y_arg):
+      for x in range(max_x_arg):
+        print ("IAMHERE-looping")
+        # slave = puresignal(slave_process)(grid, x, y) #TODO: Change this to remote_exec
+        slave = gw.remote_exec(slave_process, old_grid=grid)
+        # print ("IAMHERE-looping2")
+        slave.send((x, y))
+        slaves.append(slave)
+        slaves_cords.append((x, y))
+    print("Launched workers. Waiting on results")
+    # Update state array
+    for i in range(len(slaves)):
+      slave = slaves[i]
+      x, y = slaves_cords[i]
+      val = slave.receive() #TODO: CHange this to receive
+      grid[y][x] = val
 
-  # Run next iteration
-  return iterate(gw, iteration + 1, max_iterations, grid, max_y_arg, max_x_arg)
+    iteration += 1
 
 if __name__ == "__main__":
   import time
